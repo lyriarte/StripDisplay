ced9384f5ddc1d30249c2331372ac44bda7c7a44
Luc Yriarte <luc.yriarte@thingagora.org>
2021-03-20 00:00:24 +0100

    Text / bitmap display

--- StripDisplay.cpp
+++ StripDisplay.cpp
@@ -13,7 +13,8 @@ StripDisplay::StripDisplay(int gpio, int w, int h, int wrap, CRGB *leds) {
 	fg = CRGB(1,1,1);
 }
 
-void StripDisplay::setup() {
+void StripDisplay::setup(XBMFont* fontP) {
+	setFont(fontP);
 	pinMode(gpio, OUTPUT);
 }
 
@@ -40,10 +41,12 @@ CRGB * StripDisplay::getLeds() {
 
 void StripDisplay::setFont(XBMFont* fontP) {
 	this->fontP = fontP;
+	newTextBitmap();
 }
 
 void StripDisplay::setText(String text) {
 	this->text = text;
+	newTextBitmap();
 }
 
 void StripDisplay::setAlignment(int align) {
@@ -58,4 +61,92 @@ void StripDisplay::setFgColor(CRGB fg) {
 	this->fg = fg;
 }
 
+
+
+void StripDisplay::newTextBitmap() {
+	if (bmp != NULL)
+		BMP_Free(bmp);
+	int textWidth = text.length() * fontP->getWidth();
+	int textHeight = fontP->getHeight();
+	bmp = BMP_Create(max(w, textWidth), max(h, textHeight), 24);
+	if (bmp == NULL)
+		Serial.println("Bitmap allocation failed");
+}
+
+void StripDisplay::fillBitmap(unsigned int x0, unsigned int y0, unsigned int dx, unsigned int dy, CRGB crgb) {
+	for (unsigned int x=x0; x<x0+dx; x++)
+		for (unsigned int y=y0; y<y0+dy; y++)
+			BMP_SetPixelRGB(bmp, x, y, crgb.r, crgb.g, crgb.b);
+}
+
+void StripDisplay::blitBitmap(int i0, int ox, int oy, int dx, int dy) {
+	int i=i0, ix, iy;
+	int width = min((int)BMP_GetWidth(bmp),ox+dx);
+	int height = min((int)BMP_GetHeight(bmp),oy+dy);
+	byte r,g,b;
+	if (wrap == WRAP_LINES) {
+		for (int y=0; y<h; y++) {
+			iy = y+oy;
+			for (int x=0; x<w;x++) {
+				ix = x+ox;
+				if (ix>=0 && iy>=0 && ix<width && iy<height) {
+					BMP_GetPixelRGB(bmp,ix,iy,&r,&g,&b);
+					if (y%2 == 0) 
+						leds[i] = CRGB(r, g, b);		   
+					else 
+						leds[w*(int)(i/w) + w - ((i%w) + 1)] = CRGB(r, g, b);		   
+				}
+				++i;
+			}
+		}
+	}
+	else {
+		for (int x=0; x<w;x++) {
+			ix = x+ox;
+			for (int y=0; y<h; y++) {
+				iy = y+oy;
+				if (ix>=0 && iy>=0 && ix<width && iy<height) {
+					BMP_GetPixelRGB(bmp,ix,iy,&r,&g,&b);
+					if (x%2 == 0) 
+						leds[i] = CRGB(r, g, b);		   
+					else 
+						leds[h*(int)(i/h) + h - ((i%h) + 1)] = CRGB(r, g, b);		   
+				}
+				++i;
+			}
+		}
+	}
+}
+
+void StripDisplay::renderText(unsigned int x0, unsigned int y0, CRGB crgb) {
+	unsigned int x = x0;
+	for (int i=0; i<text.length(); i++) {
+		char c = text.charAt(i);
+		for (int j=0; j<fontP->getWidth(); j++) {
+			for (int k=0; k<fontP->getHeight(); k++)
+				if (fontP->getPixel(c,k,j))
+					BMP_SetPixelRGB(bmp, x+j, y0+k, crgb.r, crgb.g, crgb.b);
+		}
+		x += fontP->getWidth();
+	}
+}
+
+void StripDisplay::displayText(int offset) {
+	int i0 = 0;
+	int textWidth = text.length() * fontP->getWidth();
+	int textHeight = fontP->getHeight();
+	int width = min(w, textWidth);
+	int height = min(h, textHeight);
+	if (align == ALIGN_CENTER)
+		i0 = (w-width)/2;
+	else if (align == ALIGN_RIGHT)
+		i0 = (w-width);
+	// shift alignment on even columns
+	if (align != ALIGN_LEFT && wrap == WRAP_COLUMNS)
+		i0 = 2 * (int)(i0/2) * h;
+	fillBitmap(0, 0, (int)BMP_GetWidth(bmp), (int)BMP_GetHeight(bmp), bg);
+	blitBitmap(0, 0, 0, (int)BMP_GetWidth(bmp), (int)BMP_GetHeight(bmp));
+	renderText(0, 0, fg);
+	blitBitmap(i0, offset, 0, width, height);
+}
 
--- StripDisplay.h
+++ StripDisplay.h
@@ -9,6 +9,12 @@
 
 
 enum {
+	ALIGN_LEFT,
+	ALIGN_CENTER,
+	ALIGN_RIGHT
+};
+
+enum {
 	WRAP_LINES,
 	WRAP_COLUMNS
 };
@@ -20,7 +26,7 @@ class StripDisplay {
 
 	StripDisplay(int gpio, int w, int h, int wrap, CRGB *leds);
 
-	void setup();
+	void setup(XBMFont* fontP);
 
 	int getGpio();
 	int getWidth();
@@ -33,6 +39,12 @@ class StripDisplay {
 	void setAlignment(int align);
 	void setBgColor(CRGB bg);
 	void setFgColor(CRGB fg);
+
+	void newTextBitmap();
+	void fillBitmap(unsigned int x0, unsigned int y0, unsigned int dx, unsigned int dy, CRGB crgb);
+	void blitBitmap(int i0, int ox, int oy, int dx, int dy);
+	void renderText(unsigned int x0, unsigned int y0, CRGB crgb);
+	void displayText(int offset=0);
 
 	private:
 
